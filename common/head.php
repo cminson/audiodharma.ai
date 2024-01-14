c0377b810c0dfc7abbe05604236b39c9ac8bb839
cminson <christopherminson@icloud.com>
2024-01-14 22:09:00 +0000

    .

--- common/head.php
+++ common/head.php
@@ -4,7 +4,7 @@ print '
 <html lang="en">
 
 <head>
-    <title>Audiodharma AI</title>
+    <title>Buddhism Meets Artifical Intelligence</title>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
